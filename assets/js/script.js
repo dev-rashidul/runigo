99950bceacfb609457aaf40c950ac67eef74ffc3
dev-rashidul <rashidul.contact@gmail.com>
2022-11-19 22:21:42 +0600

    Benefits and Testimonial Section Done

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+// Owl Carousel for Slider
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    autoplay : true,
+    autoplayTimeout: 2000,
+    navText: ['<img src="/assets/images/arrow-left.png"/>', '<img src="/assets/images/arrow-right.png"/>'],
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
